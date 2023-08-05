68ca30f9751e7a780bfa5268167873dcbe702c40
Swifterhtmler <130903460+Swifterhtmler@users.noreply.github.com>
2023-08-05 17:38:20 +0000

    added some js

---
+++ recorder/src/test.js
@@ -0,0 +1,19 @@
+
+
+const constant = 12
+
+const variable = 5
+
+
+  if (constant >= variable) {
+    console.table("50% passed");
+    console.table("70% passed");
+    console.table("90% passed");
+    console.table("100% passed");
+  } else if (constant >= variable && constant == 12){
+    console.log("100% passed");
+  } else {
+    console.log("something went wong error 404");
+  
+  }
+
